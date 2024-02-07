54e3f57c610e1649c9223dd728d8fd9b814eedb6
Mateusz Buturla <52802446+mateuszbuturla@users.noreply.github.com>
2024-02-07 17:48:20 +0100

    Refactor wall

--- Scripts/Playfield/Playfield.gd
+++ Scripts/Playfield/Playfield.gd
@@ -2,6 +2,7 @@ extends Node
 class_name Playfield
 
 var tileScene = preload("res://Scenes/Playfield/Tile.tscn")
+var wallEntityScene = preload("res://Scenes/Entities/Obstacle/WallEntity.tscn")
 
 var cellSize: Vector2i = Vector2i(18, 18)
 var size: Vector2i = Vector2i(5, 5)
@@ -13,7 +14,15 @@ func _ready():
 		for y in range(0, size.y):
 			var pos = Vector2i(x, y)
 			var tile: Tile = tileScene.instantiate()
-			tile.createTile(x + y, pos, self, Utils.randomInt(0, 10) >= 9)
+			tile.createTile(x + y, pos, self)
+			
+			if Utils.randomInt(0, 10) >= 9:
+				CreateWall(tile)
+			
 			add_child(tile)
 			
 			tiles[pos] = tile
+
+func CreateWall(tile: Tile):
+	var wall: Entity = wallEntityScene.instantiate()
+	tile.appendEntity(wall)
--- Scripts/Playfield/Tile.gd
+++ Scripts/Playfield/Tile.gd
@@ -3,36 +3,23 @@ class_name Tile
 
 @export var sprite: Sprite2D
 
-var groundTexture: Texture2D = preload("res://Textures/Playfield/GroundTexture.png")
-var obstacleTexture: Texture2D = preload("res://Textures/Playfield/ObstacleTexture.png")
-
 var playfield: Playfield
 var index: int
 var pos: Vector2i
-var entities: Array[Node2D] #TODO Change Node2D type
-var isObstacle: bool
+var entities: Array[Entity] 
 var isHovered: bool
 
 func createTile(index: int, pos: Vector2i, playfield: Playfield, isObstacle = false):
 	self.index = index
 	self.pos = pos
 	self.entities = []
-	self.isObstacle = isObstacle
 	self.playfield = playfield
 	isHovered = false
 	setPosition()
-	setTexture()
 	subscribeSignals()
 
 func setPosition():
 	global_position = Vector2(pos.x * playfield.cellSize.x, pos.y * playfield.cellSize.y)
-
-func setTexture():
-	if isObstacle:
-		sprite.texture = obstacleTexture
-		return
-		
-	sprite.texture = groundTexture
 
 func subscribeSignals():
 	mouse_entered.connect(handleMouseEntered)
@@ -49,3 +36,8 @@ func handleMouseExited():
 func _physics_process(_delta: float):
 	if isHovered && Input.is_action_just_pressed("LeftMouse"):
 		GlobalSignals.PlayfieldTileClickSignal.emit(index, pos)
+
+func appendEntity(entity: Entity):
+	entity.position = Vector2.ZERO
+	add_child(entity)
+	entities.push_back(entity)
